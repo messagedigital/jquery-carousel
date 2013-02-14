a4cb5af64187ffeb3a0b82748bc0480a741e19c5
Joe Holdcroft <joe@message.co.uk>
2013-02-14 15:42:09 +0000

    Starting work on the internal plugin code

--- js/carousel.jquery.js
+++ js/carousel.jquery.js
@@ -1,32 +1,112 @@
 (function( $ ){
 	var methods = {
 		init : function(options) {
-			var defaults = {
-				'arrows'    : true,    // Show arrow controls
-				'indicators': true,    // Show position indicators / controls
-				'infinite'  : false,   // Imitate an infinite carousel (no start and end)
-				'animation' : 'slide', // Animation to use for moving between slides
-				'speed'     : 100,     // Speed in milliseconds for the animation
-				'interval'  : false,   // Interval for automatically moving between slides
+			var self     = this,
+				defaults = {
+					arrows    : false,   // Show arrow controls
+					indicators: true,    // Show position indicators / controls
+					infinite  : false,   // Imitate an infinite carousel (no start and end)
+					animation : 'slide', // Animation to use for moving between slides
+					speed     : 100,     // Speed in milliseconds for the animation
+					interval  : false,   // Interval for automatically moving between slides
 
-				'onChangeStart': false, // Callback for the start of the slide change animation
-				'onChangeEnd'  : false, // Callback for the end of the slide change animation
-				'onComplete'   : false, // Callback for the end of the slides being reached
-				'onHover'      : false, // Callback for when a user hovers over the carousel
-				'onInteraction': false  // Callback for when a user interacts with any controls
-			},
+					onChangeStart: false, // Callback for the start of the slide change animation
+					onChangeEnd  : false, // Callback for the end of the slide change animation
+					onComplete   : false, // Callback for the end of the slides being reached
+					onInteraction: false  // Callback for when a user interacts with any controls
+				},
 				settings = $.extend({}, defaults, options);
 
-			return this.each(function() {
-				var $this = $(this);
+			/**
+			 * Checks if a variable is a valid object with named properties
+			 * passed in the 'arrows' option.
+			 *
+			 * The object must have a property named 'next' and one either named
+			 * 'prev' or 'previous'. The value of both of these must be a jQuery
+			 * object.
+			 *
+			 * @param  mixed input The variable to check
+			 * @return boolean     True if the input is valid, false otherwise
+			 */
+			function isArrowControlObject(input)
+			{
+				if (typeof input !== 'object') {
+					return false;
+				}
 
-				$this.data('carousel', settings);
+				if (!input.hasOwnProperty('next') || !(input.next instanceof jQuery)) {
+					return false;
+				}
+
+				if (!input.hasOwnProperty('previous') || !(input.previous instanceof jQuery)) {
+					if (!input.hasOwnProperty('prev') || !(input.prev instanceof jQuery)) {
+						return false;
+					}
+				}
+
+				return true;
+			}
+
+			return this.each(function() {
+				var $this = $(this),
+					state = {
+						settings  : settings,
+						indicators: false,
+						arrows    : {
+							previous: false,
+							next    : false
+						},
+						interval  : false
+					};
+
+				// Initialise arrow controls
+				if (settings.arrows !== false) {
+					// An object with named properties for next & previous/prev
+					if (isArrowControlObject(settings.arrows)) {
+						state.arrows.next     = settings.arrows.next;
+						state.arrows.previous = (settings.arrows.hasOwnProperty('previous')) ? settings.arrows.previous : settings.arrows.prev;
+					}
+					// An array with two elements, the first is assumed the prev control
+					else if (typeof settings.arrows === 'object' && settings.arrows.length === 2) {
+						state.arrows.next     = settings.arrows[1];
+						state.arrows.previous = settings.arrows[0];
+					}
+					// Generate the arrows
+					else if (settings.arrows === true) {
+						state.arrows.next     = $('<p class="arrow next">Next</p>').prependTo($this);
+						state.arrows.previous = $('<p class="arrow previous">Previous</p>').prependTo($this);
+					}
+
+					// Set events for the arrow controls
+					state.arrows.next.on('click.carousel', function() {
+						methods.goToNext.call(self);
+					});
+					state.arrows.previous.on('click.carousel', function() {
+						methods.goToPrevious.call(self);
+					});
+				}
+
+				console.log(state);
+
+				// if (typeof settings.onHover === 'function') {
+				// 	$this.on('mouseover', settings.onHover);
+				// }
+
+				// add arrows if required (and put in data)
+				// add indicators if required (and put in data)
+				// set up the interbal if required (and put in data)
+				// set up events for hover and interaction
+				// add class of initialised once done
+				//
+
+				// Save state on the element for use later
+				$this.data('carousel', state);
 			});
 		},
 		goTo : function(index) {
 			console.log('calling goTo with ' + index);
 			return this.each(function() {
-				var settings = $(this).data('carousel');
+				var settings = $(this).data('carousel').settings;
 
 				console.log(settings);
 			});
@@ -37,7 +117,13 @@
 		goToEnd : function() {
 			// call goTo with the last index
 		},
-		start : function(interval) {
+		goToNext : function() {
+			console.log('go to next');
+		},
+		goToPrevious : function() {
+			console.log('go to prev');
+		},
+		start : function() {
 			// start the interval
 		},
 		stop : function() {
@@ -46,21 +132,6 @@
 	};
 
 	$.fn.carousel = function(method) {
-			// var settings = {
-			// 	'arrows'    : true,    // Show arrow controls
-			// 	'indicators': true,    // Show position indicators / controls
-			// 	'infinite'  : false,   // Imitate an infinite carousel (no start and end)
-			// 	'animation' : 'slide', // Animation to use for moving between slides
-			// 	'speed'     : 100,     // Speed in milliseconds for the animation
-			// 	'interval'  : false,   // Interval for automatically moving between slides
-
-			// 	'onChangeStart': false, // Callback for the start of the slide change animation
-			// 	'onChangeEnd'  : false, // Callback for the end of the slide change animation
-			// 	'onComplete'   : false, // Callback for the end of the slides being reached
-			// 	'onHover'      : false, // Callback for when a user hovers over the carousel
-			// 	'onInteraction': false  // Callback for when a user interacts with any controls
-			// };
-
 		// Method calling logic
 		if (methods[method]) {
 			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
@@ -73,7 +144,5 @@
 		// methods for goTo(index) / goToStart / goToEnd
 		// methods for stop / start with intervals
 		// ensure widths are re-calculated on orientation change
-
-		return this; // maintain chainability
 	};
 })( jQuery );
---
+++ readme.md
@@ -0,0 +1,54 @@
+# Carousel jQuery Plugin
+
+This jQuery plugin provides carousel functionality typical for the sites we build.
+
+## Usage
+
+### Options
+
+The available options are as follows:
+
+**TODO: add option list with descriptions here**
+
+#### `arrows`
+
+This defines whether or not previous/next arrow controls should be used. If passed as `false`, no arrow controls will be generated or used.
+
+If passed as `true`, the arrow controls will be generated and added to the DOM inside the target element as the following:
+
+	<p class="arrow previous">Previous</p>
+	<p class="arrow next">Next</p>
+
+If you wish to define your own HTML for the arrow controls, you can pass jQuery selectors for the arrow controls. These can either be passed as an array with two elements (the first is assumed as the previous control, and the second is assumed as the next control):
+
+	$('.gallery').carousel({
+		arrows: [$('.gallery .myPrevButton'), $('.gallery .myNextButton')]
+	});
+
+Or as an object with named properties of `next` and either `prev` or `previous`. Both of the following are valid:
+
+	$('.gallery').carousel({
+		arrows: {
+			prev: $('.gallery .myPrevButton'),
+			next: $('.gallery .myNextButton')
+		}
+	});
+
+	$('.gallery').carousel({
+		arrows: {
+			previous: $('.gallery .myPrevButton'),
+			next    : $('.gallery .myNextButton')
+		}
+	});
+
+The `arrows` option defaults to `false`.
+
+### Prerequisites
+
+**TODO: mention what markup it expects for the element**
+
+## Roadmap
+
+### Feature ideas
+
+* Add `randomise` option that will shuffle the carousel slides when initialising.
